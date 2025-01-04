93faeba4d5c137b2eb45fc06d916a11857fbd8db
andrey <domaratskiy1990@gmail.com>
2025-01-04 19:15:57 +0200

    add env

--- app/page.js
+++ app/page.js
@@ -5,11 +5,7 @@ import GalleryClient from "./component/galleryClient";
 import cloudinary from "cloudinary";
 
 export default async function Gallery() {
-  cloudinary.v2.config({
-    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+
 
   const cloudinaryData = await cloudinary.v2.search
     .expression("resource_type:image")
