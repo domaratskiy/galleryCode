a69b62c87b3af8417d22068429fe6c217128e1fe
andrey <domaratskiy1990@gmail.com>
2025-01-04 18:13:47 +0200

    ADD CSS

--- app/component/modal.js
+++ app/component/modal.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Modal({ selectedImage, onClose }) {
   if (!selectedImage) return null;
 
-  const largeImageUrl = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/w_1200,h_800,c_limit,q_auto:best/${selectedImage}`;
+  const largeImageUrl = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/w_1200,h_800,c_limit,q_auto:best/${selectedImage}`;
 
   return (
     <div className="modal" onClick={onClose}>
