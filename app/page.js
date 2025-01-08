38d9978d4cbaea1c72f25c727d286148898f6568
andrey <domaratskiy1990@gmail.com>
2025-01-08 21:12:57 +0200

    not last mod

--- app/page.js
+++ app/page.js
@@ -3,12 +3,7 @@ import GalleryBtn from "./component/uploadBtn";
 import cloudinary from "cloudinary";
 
 export default async function Page() {
-  // Конфигурация Cloudinary
-  cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+
 
   // Запрос изображений
   const cloudinaryData = await cloudinary.v2.search
